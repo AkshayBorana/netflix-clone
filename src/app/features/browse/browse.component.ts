e30832815426f045738f9c690296c769ad1a65a3
AkshayBorana <akshayborana1@gmail.com>
2025-05-08 15:03:01 -0400

    feat: TECH-6 Built banner component to display an iframe.

--- src/app/features/browse/browse.component.ts
+++ src/app/features/browse/browse.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../core/components/header/header.component';
+import { BannerComponent } from '../../core/components/banner/banner.component';
 
 @Component({
   selector: 'app-browse',
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.scss',
-  imports: [HeaderComponent]
+  imports: [HeaderComponent, BannerComponent]
 })
 
 export class BrowseComponent {
