a2a3acb9454eb676ab2a19d6215e20c81060aee4
AkshayBorana <akshayborana1@gmail.com>
2025-05-10 13:15:31 -0400

    feat: TECH-8 Added args to make clip text the pipe dynamic.

--- src/app/shared/pipes/clip-text.pipe.ts
+++ src/app/shared/pipes/clip-text.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'clipText',
 })
 export class ClipTextPipe implements PipeTransform {
-  transform(value: string): string {
-    return value.substring(0, 140);
+  transform(value: string, args: number): string {
+    return `${value.substring(0, args)}...`;
   }
 }
