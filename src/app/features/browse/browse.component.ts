947beac0550f2dd14d8573e5c47d4dcd43627727
AkshayBorana <akshayborana1@gmail.com>
2025-05-10 13:05:15 -0400

    feat: TECH-8 Implemented carousel component to display movies.

--- src/app/features/browse/browse.component.ts
+++ src/app/features/browse/browse.component.ts
@@ -1,16 +1,22 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { HeaderComponent } from '../../core/components/header/header.component';
 import { BannerComponent } from '../../core/components/banner/banner.component';
 import { MovieService } from '../../shared/services/movie.service';
+import { MovieCarouselComponent } from '../../shared/components/movie-carousel/movie-carousel.component';
+import { MovieList } from '../../core/models/movies.interface';
 
+// const components = [HeaderComponent, BannerComponent, MovieCarouselComponent];
 @Component({
   selector: 'app-browse',
+  imports: [HeaderComponent, BannerComponent, MovieCarouselComponent],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.scss',
-  imports: [HeaderComponent, BannerComponent],
 })
 export class BrowseComponent implements OnInit {
   private moviesService = inject(MovieService);
+
+  moviesList = signal<MovieList[]>([]);
+  setMoviesList = computed(() => this.moviesList());
 
   ngOnInit(): void {
     this.getMovies();
@@ -20,7 +26,20 @@ export class BrowseComponent implements OnInit {
     try {
       const data = await this.moviesService.fetchMovies();
       console.log(data);
+      if(data && data['results'] && data['results']?.length) {
+        const result = data.results.map((movie: MovieList) => {
+          return {
+            ...movie,
+            poster_path: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+          }
+        })
+        console.log(result)
+        this.moviesList.set(result);
+      } else {
+        this.moviesList.set([]);
+      }
     } catch (error) {
+      this.moviesList.set([]);
       console.log(error);
     }
   }
---
+++ src/app/shared/components/movie-carousel/movie-carousel.component.ts
@@ -0,0 +1,90 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  input,
+  viewChild,
+} from '@angular/core';
+import { MovieList } from '../../../core/models/movies.interface';
+// import {input} from '@angular/core`;
+import { Swiper } from 'swiper';
+
+@Component({
+  selector: 'app-movie-carousel',
+  templateUrl: './movie-carousel.component.html',
+  styleUrls: ['./movie-carousel.component.scss'],
+  standalone: true,
+})
+export class MovieCarouselComponent implements OnInit, AfterViewInit {
+  videoContents = input<MovieList[]>();
+  title = input('');
+  selectedContent: any;
+  image = 'movie not found';
+
+  swiperContainer = viewChild<ElementRef>('swiperContainer');
+
+  constructor() {
+    console.log(this.videoContents);
+  }
+
+  ngOnInit(): void {
+    console.log(this.videoContents);
+    // this.initSwiper();
+  }
+
+  ngAfterViewInit(): void {
+    this.initSwiper();
+  }
+
+  private initSwiper() {
+    return new Swiper(this.swiperContainer()?.nativeElement, {
+      slidesPerView: 3,
+      slidesPerGroup: 2,
+      centeredSlides: true,
+      loop: true,
+      breakpoints: {
+        600: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          spaceBetween: 5,
+          centeredSlides: true,
+        },
+        900: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+          spaceBetween: 5,
+          centeredSlides: true,
+        },
+        1200: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+          spaceBetween: 5,
+          centeredSlides: false,
+        },
+        1500: {
+          slidesPerView: 5,
+          slidesPerGroup: 5,
+          spaceBetween: 5,
+          centeredSlides: false,
+        },
+        1800: {
+          slidesPerView: 5,
+          slidesPerGroup: 6,
+          spaceBetween: 5,
+          centeredSlides: false,
+        },
+      },
+    });
+  }
+
+  setHoverMovie(movie: any): void {
+    console.log(movie);
+  }
+
+  clearHoverMovie(): void {}
+}
+
+// function input<T>(arg0: never[]): any[] {
+//   throw new Error('Function not implemented.');
+// }
